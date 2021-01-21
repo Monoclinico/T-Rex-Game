c79dd536569cade08f6e4117920a9d5ca208dd31
Matheus Sanches <matheussanchesleme@gmail.com>
2021-01-20 22:53:22 -0300

    +background mudado; +melhoramento do pulo; +melhorias gerais

--- script.js
+++ script.js
@@ -17,49 +17,52 @@ function jump(){
     isJumping = true;
 
     let upInterval = setInterval(()=>{
-        if (position >= 150){
+
+        if (position >= 160){
             clearInterval(upInterval);
             let downInterval = setInterval(()=>{
                 if(position <=0 ){
                     clearInterval(downInterval);
                     isJumping = false;
                 }else{
-                    position -= 20;
+                    position -= 9;
                     dino.style.bottom = position + "px";
                 }
-            },20)
+            },3)
         }else{
 
         
-        position += 20;
+        position += 9;
         dino.style.bottom = position + "px";
     }
-    },20);
+    },3);
 
 }
 
 function createCactus(){
     const cactus = document.createElement("div");
-    let cactusPosition = 1000;
-    let randomTime = Math.random() *6000;
-
+    let cactusPosition = 2000;
+    let randomTime;
+    do {
+        randomTime = Math.random() *6000;
+    }while(randomTime<900);    
     cactus.classList.add("cactus");
-    cactus.style.left = 1000+ "px";
+    cactus.style.left = cactusPosition+ "px";
     background.appendChild(cactus);
 
     let leftInterval = setInterval(()=>{
         if (cactusPosition < -60){
             clearInterval(leftInterval);
-            background.removeChild(cactus);
+            background.removeChild(cactus);/*
         }else if (cactusPosition > 0 && cactusPosition < 60 && position <60){
             clearInterval(leftInterval);
-            document.body.innerHTML= "<h1 class='game-over'>Fim de Jogo</h1>";
+            document.body.innerHTML= "<h1 class='game-over'>Fim de Jogo</h1>";*/
         }else{
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + "px";
             
         }
-    },20);
+    },1);
     setTimeout(createCactus,randomTime);
 }
 createCactus();
