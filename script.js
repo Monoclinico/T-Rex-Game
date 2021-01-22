b2fdfbcd7ec0750d621d27bbb61d884af8eca4bf
Matheus Sanches <matheussanchesleme@gmail.com>
2021-01-22 15:53:13 -0300

    +alteração na imagem da pedra; +aperfeiçoamento nos temporizadores; +melhoramentos

--- script.js
+++ script.js
@@ -22,59 +22,60 @@ function handleKeyUp(event){
 
 function jump(){
     isJumping = true;
+    let isUp =true;
     dino.style.animationPlayState = "paused";
     let upInterval = setInterval(()=>{
-        if (position >= 180){
-            clearInterval(upInterval);
-            let downInterval = setInterval(()=>{
-                if(position <=0 ){
-                    clearInterval(downInterval);
-                    isJumping = false;
-                    dino.style.animationPlayState = "running";
-
+        if(!isLose){
+            if (position >= 175){
+                isUp = false;
+            }
+            if(position <=17 && !isUp){
+                clearInterval(upInterval);
+                isJumping = false;
+                dino.style.animationPlayState = "running";
+            } else{      
+                if(isUp){
+                    position += 19;
                 }else{
-                    if (!isLose){
-                        position -= 10;
-                        dino.style.bottom = position + "px";
-                    }
+                    position -= 18;
                 }
-            },3)
-        }else{
-            if (!isLose){
-                position += 11;
                 dino.style.bottom = position + "px";
             }
         }
-    },3);
+    },30);
+}
 
+function randomBackgrondCactus(){
+    let randomBackground;
+    randomBackground = (Math.random() *1000);
+    if (randomBackground <= 300){
+        return "url(./cactus1.png)";
+    }else if(randomBackground > 300 && randomBackground <= 600){
+        return "url(./rock.png)";
+    }else{
+        return "url(./cactus2.png)";
+    }
 }
 
 function createCactus(){
     
     if(!isLose){
         const cactus = document.createElement("div");
-        let cactusPosition = 1500;
-   
+        let cactusPosition = 1300 + (Math.random() *1000);
         cactus.classList.add("cactus");
         cactus.style.left = cactusPosition+ "px";
-        let randomBackground;
-        randomBackground = (Math.random() *1000);
-        if (randomBackground <= 300){
-            cactus.style.backgroundImage = "url(./cactus1.png)";
-        }else if(randomBackground > 300 && randomBackground <= 600){
-            cactus.style.backgroundImage = "url(./rock.png)";
-        }else{
-            cactus.style.backgroundImage = "url(./cactus2.png)";
-        }
+        cactus.style.backgroundImage = randomBackgrondCactus();
         background.appendChild(cactus);
 
+        
         let leftInterval = setInterval(()=>{
             if (cactusPosition < -60){
                 clearInterval(leftInterval);
                 background.removeChild(cactus);
-            }else if (cactusPosition > 110 && cactusPosition < 168 && position <67){
+                createCactus();
+                
+            }else if (cactusPosition > 110 && cactusPosition < 160 && position <67){
                 clearInterval(leftInterval);
-                clearInterval(createCactusInterval);
                 clearInterval(addScoreInterval);
                 isLose = true;
                 background.style.animationPlayState = "paused";
@@ -82,19 +83,23 @@ function createCactus(){
                 const endGame = document.createElement("div");
                 background.appendChild(endGame);
                 endGame.innerHTML = "Game Over";
-                endGame.style = "color: red;text-align: center;font-family: Arial; font-weight: 900;font-size: 8ch;;width:auto;height: 150px; position:relative; transform: translate(-50%, -50%); top:50%; left: 50%";
+                endGame.style = "z-index:1000;color: red;text-align: center;font-family: Arial; font-weight: 900;font-size: 8ch;;width:auto;height: 150px; position:relative; transform: translate(-50%, -50%); top:50%; left: 50%";
                 const restartGame = document.createElement("div");
                 background.appendChild(restartGame);
                 restartGame.innerHTML = "Press space or key up to restart";
-                restartGame.style = "color: black;text-align: center;font-family: Arial; font-weight: 900;font-size: 4ch;;width:auto;height: 150px; position:relative; transform: translate(-50%, -50%); top:50%; left: 50%";
+                restartGame.style = "z-index:1000;color: black;text-align: center;font-family: Arial; font-weight: 900;font-size: 4ch;;width:auto;height: 150px; position:relative; transform: translate(-50%, -50%); top:50%; left: 50%";
 
             }else{
                 cactusPosition -= velocityCactus;
                 cactus.style.left = cactusPosition + "px";
                 
             }
-        },1);
-        setInterval(()=>{if(isLose){clearInterval(leftInterval);}},100);
+            if(isLose){
+                clearInterval(leftInterval);
+            }
+
+        },30);
+ 
     }
 }
 
@@ -104,22 +109,14 @@ function addScore(){
     if (score % 100 == 0){
         velocityCactus+=1;
         if (velocityBackground>=30){
-            velocityBackground-=10;
+            velocityBackground-=8;
         }
         background.style.animationDuration = velocityBackground+"s";
     }
 }
 
-function callCreateCactus(){
-    if(!isLose){
-        let randomTime;
-        randomTime = (Math.random() *1000);
-        let createCactusTimeout = setTimeout(createCactus,randomTime);
-    }
-}
-
 let addScoreInterval = setInterval(addScore,100);
 
-let createCactusInterval = setInterval(callCreateCactus,1500);
+createCactus();
 
-document.addEventListener("keyup", handleKeyUp);
+document.addEventListener("keydown", handleKeyUp);
